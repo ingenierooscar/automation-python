4a3b92232cc0bd2eaee09d9330b129355f962412
ingenierooscar <dante512@hotmail.com>
2022-10-19 21:27:34 -0500

    crete and write files

--- fileIO.py
+++ fileIO.py
@@ -24,3 +24,17 @@ for line in f:
         print(line)
 
 f.close()
+
+ #create files and write in it
+f = open('inputfile.txt', 'r') 
+passFile = open('PassDile.txt', 'w')
+failFile = open('FailFile.txt', 'w')
+for line in f:
+    line_split = line.split()
+    if line_split[2] == 'P':
+        passFile.write(line)
+    else:
+        failFile.write(line)    
+f.close()
+passFile.close()
+failFile.close()
